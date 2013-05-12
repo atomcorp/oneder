06cb37af0437a168a609ee5413cc3e80ca6ce3b6
atomcorp <thomasmaxwellsmith@gmail.com>
2013-05-12 20:56:44 +0100

    Just before rearranging the UL

--- generator.js
+++ generator.js
@@ -2,38 +2,33 @@
 this.uiSelect = function(){
 	var length = $("#ui .choiceNames li").length;
 	var ran = Math.floor(Math.random()*length) + 1;
-	while (length >= 1) {
-		$('#ui li').hide();
-		length--;
-		// alert('hello');
-	  };
-	$("#ui li:nth-child(" + ran + ")").show();
+	$('#ui li').css('display','none');
+	$("#ui li:nth-child(" + ran + ")").css('display','inline');
 };
 
 this.clientSelect = function(){
 	var length = $("#client .choiceNames li").length;
 	var ran = Math.floor(Math.random()*length) + 1;
-	while (length >= 1) {
-		$('#client li').hide();
-		length--;
-		// alert('hello');
-	  };
-	$("#client li:nth-child(" + ran + ")").show();
+	$('#client li').css('display','none');
+	$("#client li:nth-child(" + ran + ")").css('display','inline');
 };
 
 this.demandsSelect = function(){
 	var length = $("#demands .choiceNames li").length;
 	var ran = Math.floor(Math.random()*length) + 1;
-	while (length >= 1) {
-		$('#demands li').hide();
-		length--;
-		// alert('hello');
-	  };
-	$("#demands li:nth-child(" + ran + ")").show();
+	// while (length >= 1) {
+	$('#demands li').css('display','none');
+	// length--;
+	// alert('hello');
+	// };
+	$("#demands li:nth-child(" + ran + ")").css('display','inline');
 };
 
 $(document).ready(function(){	
-	$('#mangle').click(function(){
+		uiSelect();
+		clientSelect();
+		demandsSelect();
+	$('#pushTheButton').click(function(){
 		uiSelect();
 		clientSelect();
 		demandsSelect();
